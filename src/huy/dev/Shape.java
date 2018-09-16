5bed33d9817a04e3e145df8d2290f9fce4ed6ec6
unknown <huy.sirmaster@gmail.com>
2018-09-16 13:47:20 +0700

    ckass

--- src/huy/dev/Shape.java
+++ src/huy/dev/Shape.java
@@ -1,10 +1,7 @@
 package huy.dev;
 
-public class Shape {
+public abstract class Shape {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		public abstract double dientich();
 
 }
