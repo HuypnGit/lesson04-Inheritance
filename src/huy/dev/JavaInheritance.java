7fd1d3dc06baa6c02bb99da220fb8e5314256f6e
unknown <huy.sirmaster@gmail.com>
2018-09-16 14:01:50 +0700

    add

--- src/huy/dev/JavaInheritance.java
+++ src/huy/dev/JavaInheritance.java
@@ -1,10 +1,29 @@
 package huy.dev;
 
+import huy.dev.Circle;
+import huy.dev.Shape;
+import huy.dev.Square;
+import huy.dev.Triangle;
+
 public class JavaInheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Shape shape[] =new Shape[10];
+		shape[0]= new Square(1,2){};
+		shape[1]= new Square(1,2){};
+		shape[2]= new Circle(5){};
+		shape[3]= new Circle(7){};
+		shape[4]= new Triangle(5,2,4){};
+		shape[5]= new Triangle(3,2,4){};
+		shape[6]= new Triangle(2,2,4){};
+		shape[7]= new Square(1,2){};
+		shape[8]= new Circle(8){};
+		shape[9]= new Triangle(6,3,4){};
+	for (int i = 0;i<shape.length;i++) {
+		System.out.println("Dien tich la :"+shape[i].dientich());
+		}
+	for (int i = 0;i<shape.length;i++) {
+		}
 	}
-
 }
+
